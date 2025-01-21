fc0458229f65010b8aa2e9538e9efc699739006d
cypher4859 <obsidia_framework_notification@opaquecipher.33mail.com>
2025-01-20 21:00:39 -0500

    Added some variables and cross account assume policy

--- terraform/modules/post-account-setup-provisioning/variables.tf
+++ terraform/modules/post-account-setup-provisioning/variables.tf
@@ -8,7 +8,17 @@ variable "sub_account_id" {
   description = "value"
 }
 
-variable "student_acct_staff_administrators_role_arn" {
+variable "sub_account_staff_administrators_role_arn" {
   type = string
   description = "The Administrators role for staff in the Student Account"
+}
+
+variable "root_acct_staff_administrators_role_arn" {
+  type = string
+  description = "The Administrators Role for staff in the Root Account"
+}
+
+variable "root_acct_staff_administrators_role_name" {
+  type = string
+  description = "The Administrators Role for staff in the Root Account"
 }
